850c52fd469670f3a824d83951e8d608e8509459
septor <patrickweaver@gmail.com>
2013-02-03 10:42:05 -0500

    Dutch language file updated

--- languages/Dutch.php
+++ languages/Dutch.php
@@ -13,12 +13,15 @@ define("CANVAS_PLUGIN_04", " succesvol ge-upgraded.");
 // config.php
 define("CANVAS_CONFIG_01", "Instellingen succesvol opgeslagen.");
 define("CANVAS_CONFIG_02", "Menu Titel:<br /><i>Gebruikmaken van CANVAS_TITLE of dit veld leeg laten maakt gebruik van het taalbestand.</i>");
-define("CANVAS_CONFIG_03", "Te tonen afbeelding:");
+define("CANVAS_CONFIG_03", "Te tonen afbeelding: <br /><i>Flickr en Instagram afbeeldingen worden niet eerder getoond voordat hun iformatie is bewaard in de voorkeuren.");
 define("CANVAS_CONFIG_04", "Geen");
 define("CANVAS_CONFIG_05", "Willekeurig");
-define("CANVAS_CONFIG_06", "Afbeeldings Formaat:<br /><i>Wijdte x Hoogte, geen spaties.</i>");
+define("CANVAS_CONFIG_06", "Afbeeldings Formaat:<br /><i>Breedte x Hoogte, geen spaties.</i>");
 define("CANVAS_CONFIG_07", "Bewaar veranderingen");
 define("CANVAS_CONFIG_08", "Configureer Canvas");
+define("CANVAS_CONFIG_09", "Flickr ID:");
+define("CANVAS_CONFIG_10", "Eigen Afbeeldingen");
+define("CANVAS_CONFIG_11", "Flickr Afbeeldingen");
 
 // canvas_menu.php
 define("CANVAS_MENU_01", "Geen afbeelding geselecteerd voor weergave.");
